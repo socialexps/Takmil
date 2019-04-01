63aa850dcc0a3d20b889116c412845cdd7857b9c
socialexps <48156945+socialexps@users.noreply.github.com>
2019-04-01 10:29:22 -0700

    Adding code that uses MS Face APi to detect no. of faces
    This covers using API to detect no. of faces, and also how to read from azure blob

---
+++ Samples/FaceApi/Program.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+
+namespace FaceApi
+{
+    class Program
+    {   
+        private const string subscriptionkey = "1c0e6d5149394ceaa9c24abbcbad9eab";
+        // You must use the same region as you used to get your subscription
+        // keys. For example, if you got your subscription keys from westus,
+        // replace "westcentralus" with "westus".
+        //
+        // Free trial subscription keys are generated in the westcentralus
+        // region. If you use a free trial subscription key, you shouldn't
+        // need to change the region.
+        // Specify the Azure region
+        private const string faceEndpoint = "https://eastus.api.cognitive.microsoft.com";
+        // localImagePath = @"C:\Documents\LocalImage.jpg"
+        private const string localImagePath = @"<LocalImage>";
+
+        //private const string remoteImageUrl ="https://yadavsrorageaccount01.blob.core.windows.net/takmil/takmildemo.jpg";
+       
+        //"https://takmil.org/wp-content/uploads/2018/03/img-20180203-wa0004.jpg";
+        //"https://upload.wikimedia.org/wikipedia/commons/3/37/Dagestani_man_and_woman.jpg";
+
+        private static readonly FaceAttributeType[] faceAttributes = { FaceAttributeType.Age, FaceAttributeType.Gender };
+
+        private static int totalFaceDetected = 0;
+        private static int femaleDetected = 0;
+        private static int maleDetected = 0;
+
+        static void Main(string[] args) {
+        
+            var remoteImageUrl = storagefunc();
+            Console.WriteLine("Welcome to Face API at work ....!");
+            FaceClient faceClient = new FaceClient (
+                new ApiKeyServiceClientCredentials(subscriptionkey),
+                new System.Net.Http.DelegatingHandler[] { });
+            faceClient.Endpoint = faceEndpoint;
+            Console.WriteLine("Face being detected . . .");
+            var t1 = DetectedRemoteAsync(faceClient , remoteImageUrl);
+            
+            Task.WhenAll(t1).Wait(5000);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();    
+        }
+        // It uses the Face service client to detect faces in a remote image, referenced by a URL. 
+        // Note that it uses the faceAttributes field—the DetectedFace objects added to faceList will have the specified attributes (in this case, age and gender)
+        private static async Task DetectedRemoteAsync(FaceClient faceClient, string ImageUrl)
+        {
+            if(!Uri.IsWellFormedUriString(ImageUrl, UriKind.Absolute))
+            {
+                Console.WriteLine("\n Invalid remoteImageurl : \n {0} \n",ImageUrl);
+                return;
+            }
+
+            try
+            {
+                IList<DetectedFace> faceList = 
+                    await faceClient.Face.DetectWithUrlAsync(
+                        ImageUrl, true , false , faceAttributes);
+                DisplayAttributes(GetFaceAttributes(faceList,ImageUrl), ImageUrl);
+                Console.WriteLine("Total Face detected :: {0}",totalFaceDetected);
+                Console.WriteLine("Total Female detected :: {0}",femaleDetected);
+                Console.WriteLine("Total Male detected :: {0}",maleDetected);
+            }
+            catch (APIErrorException e)
+            {
+                Console.WriteLine(ImageUrl + ":" + e.Message);
+            }
+            
+        }
+        // the GetFaceAttributes method. It returns a string with the relevant attribute information.
+        private static string GetFaceAttributes (
+            IList<DetectedFace> faceList, string imagePath)
+            {
+                string attributes = string.Empty;
+
+                foreach (DetectedFace face in faceList)
+                {
+                    double? age = face.FaceAttributes.Age;
+                    string gender = face.FaceAttributes.Gender.ToString();
+                    attributes += gender + " " + age + "   " + "\n";
+                    if(gender == "Female")
+                        femaleDetected ++;
+                    else
+                        maleDetected ++;
+                    totalFaceDetected ++;
+                }
+                return attributes;
+            }
+        private static void DisplayAttributes(string attributes, string imageUri)
+        {
+            Console.WriteLine(imageUri);
+            Console.WriteLine(attributes + "\n");
+        }
+
+        private static string storagefunc()
+        {
+            // Retrieve the connection string for use with the application. The storage connection string is stored
+            // in an environment variable on the machine running the application called storageconnectionstring.
+            // If the environment variable is created after the application is launched in a console or with Visual
+            // Studio, the shell or application needs to be closed and reloaded to take the environment variable into account.
+            string storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=yadavsrorageaccount01;AccountKey=PBotd4hx3Z1o3VOYDvohsAhKpypGKp8c5GwdOgge0gQZGEtlFvkbdTIVoPjhw0Dm7QUD/Gc/PwlI4DE1P9yfhg==;EndpointSuffix=core.windows.net";
+            string primaryUri = "";
+            // Check whether the connection string can be parsed.
+            CloudStorageAccount storageAccount;
+            if (CloudStorageAccount.TryParse(storageConnectionString, out storageAccount))
+            {
+                // If the connection string is valid, proceed with operations against Blob storage here.
+                // List the blobs in the container.
+                //CloudStorageAccount account = CloudStorageAccount.Parse("PBotd4hx3Z1o3VOYDvohsAhKpypGKp8c5GwdOgge0gQZGEtlFvkbdTIVoPjhw0Dm7QUD/Gc/PwlI4DE1P9yfhg==");
+                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+                CloudBlobContainer container = blobClient.GetContainerReference("takmil");
+                CloudBlob blob = container.GetBlobReference("takmildemo.jpg");
+                primaryUri = blob.StorageUri.PrimaryUri.ToString();
+                Console.WriteLine(primaryUri);
+                return primaryUri;
+            }
+            else
+            {
+                // Otherwise, let the user know that they need to define the environment variable.
+                Console.WriteLine(
+                    "A connection string has not been defined in the system environment variables. " +
+                    "Add an environment variable named 'storageconnectionstring' with your storage " +
+                    "connection string as a value.");
+                Console.WriteLine("Press any key to exit the sample application.");
+                Console.ReadLine();
+                return null;
+            }
+        }
+
+}
+}
